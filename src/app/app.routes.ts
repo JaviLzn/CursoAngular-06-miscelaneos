f97f4591d64d4aa20aae93e0b31d52243d147d9f
Javier Lozano <javier_l_o@hotmail.com>
2018-09-08 15:37:31 -0500

    Se crean componentes para usuario y manejo de ruta

---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './components/home/home.component';
+
+
+const APP_ROUTES: Routes = [
+    {path: 'home', component: HomeComponent },
+    {path: 'usuario', component: UsuarioComponent },
+    {path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
